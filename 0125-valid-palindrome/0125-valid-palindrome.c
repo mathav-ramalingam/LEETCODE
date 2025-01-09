dca24681afdaf20905ed8bac8591b7d9c9ccb09a
Mathav Ramalingam <mathavramalingam1608@gmail.com>
2025-01-09 23:05:45 +0530

    Time: 0 ms (100%), Space: 8.5 MB (46.75%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.c
+++ 0125-valid-palindrome/0125-valid-palindrome.c
@@ -1,35 +1,51 @@
 bool isPalindrome(char* s) {
-    int k=0;
-    for(int i=0;i<strlen(s);i++)
-    {
-        if((s[i]>='A' && s[i]<='Z')  || (s[i] >='a' && s[i]<='z') || (s[i]>='0' && s[i]<='9'))
-        {
-            s[k++]=s[i];
+    //without lib func
+    // int k=0;
+    // for(int i=0;i<strlen(s);i++)
+    // {
+    //     if((s[i]>='A' && s[i]<='Z')  || (s[i] >='a' && s[i]<='z') || (s[i]>='0' && s[i]<='9'))
+    //     {
+    //         s[k++]=s[i];
+    //     }
+    // }
+    // s[k]='\0';
+    // printf("%s",s);
+    // int i=0;
+    // int j = strlen(s)-1;
+    // while(i<j)
+    // {
+    //     char c1 = s[i];
+    //     char c2 = s[j];
+    //     if(c1>='A' && c1<='Z')
+    //     {
+    //         c1 = c1+32;
+    //     }
+    //     if(c2>='A' && c2<='Z')
+    //     {
+    //         c2 = c2+32;
+    //     }
+    //     if(c1 != c2)
+    //     {
+    //         return false;
+    //     }
+    //     i++;
+    //     j--;
+    // }
+
+
+    int i=0,j=strlen(s);
+    while(i<j){
+        while(i<j && !isalnum(s[i])){
+            i++;
         }
-    }
-    s[k]='\0';
-    printf("%s",s);
-    int i=0;
-    int j = strlen(s)-1;
-    while(i<j)
-    {
-        char c1 = s[i];
-        char c2 = s[j];
-        if(c1>='A' && c1<='Z')
-        {
-            c1 = c1+32;
+        while(i<j && !isalnum(s[j])){
+            j--;
         }
-        if(c2>='A' && c2<='Z')
-        {
-            c2 = c2+32;
-        }
-        if(c1 != c2)
-        {
+        if(tolower(s[i])!=tolower(s[j])){
             return false;
         }
         i++;
         j--;
     }
-
     return true;
 }
