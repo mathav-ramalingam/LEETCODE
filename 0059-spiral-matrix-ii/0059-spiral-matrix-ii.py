96ae96b4cdbd23e5bae011f7bd01fd634248ced0
Mathav Ramalingam <mathavramalingam1608@gmail.com>
2025-03-02 13:48:31 +0530

    Time: 0 ms (100%), Space: 17.7 MB (93.01%) - LeetHub

--- 0059-spiral-matrix-ii/0059-spiral-matrix-ii.py
+++ 0059-spiral-matrix-ii/0059-spiral-matrix-ii.py
@@ -19,16 +19,43 @@ class Solution:
                 num = num+1
             right = right -1
 
-            for i in range(right, left - 1, -1):
-                matrix[down][i] = num
-                num += 1
-            down -= 1
+            if top <= down:
+                for i in range(right, left - 1, -1):
+                    matrix[down][i] = num
+                    num += 1
+                down -= 1
 
-            for i in range(down, top - 1, -1):
-                matrix[i][left] = num
-                num += 1
-            left += 1
+            if left <= right:
+                for i in range(down, top - 1, -1):
+                    matrix[i][left] = num
+                    num += 1
+                left += 1
   
         return matrix
+
+
+
+
+        # while left <= right and top<= down:
+        #     for i in range(left,right+1):
+        #         matrix[top][i] = num
+        #         num = num+1
+        #     top = top+1
+        #     for i in range(top , down+1):
+        #         matrix[i][right] =  num
+        #         num = num+1
+        #     right = right -1
+
+        #     for i in range(right, left - 1, -1):
+        #         matrix[down][i] = num
+        #         num += 1
+        #     down -= 1
+
+        #     for i in range(down, top - 1, -1):
+        #         matrix[i][left] = num
+        #         num += 1
+        #     left += 1
+  
+        # return matrix
 
         
