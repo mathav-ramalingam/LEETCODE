ed8b96134e5978eea6041e7b999ebd3258c05d9f
Mathav Ramalingam <mathavramalingam1608@gmail.com>
2025-03-06 21:35:30 +0530

    Time: 3 ms (25.38%), Space: 17.7 MB (91.94%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.py
+++ 0392-is-subsequence/0392-is-subsequence.py
@@ -1,6 +1,20 @@
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
 
+
+        # my code testcase fail
+        #         s ="acb" t ="ahbgdc"
+        #         Output : true
+        #         Expected :false
+
+        # count=0
+        # s_len = len(s)
+        # for char in s:
+        #     if char in t:
+        #         count=count+1       
+
+        # return count==s_len
+
         s_len = len(s)
         t_len = len(t)
         i,j = 0,0
