29ff7bfede10291c690db60accab81ed9dbfe26c
Mathav Ramalingam <158606381+mathav-ramalingam@users.noreply.github.com>
2025-06-20 14:59:41 +0530

    Time: 93 ms (7.27%), Space: 18.5 MB (77.69%) - LeetHub

--- 0838-design-linked-list/0838-design-linked-list.py
+++ 0838-design-linked-list/0838-design-linked-list.py
@@ -87,6 +87,33 @@ class MyLinkedList:
             prev += 1
 
 
+# Usingg list
+
+# class MyLinkedList:
+
+#     def __init__(self):
+#         self.arr = []
+
+#     def get(self, index: int) -> int:
+#         if index >= len(self.arr): return -1
+
+#         return self.arr[index]
+
+#     def addAtHead(self, val: int) -> None:
+#         self.arr.insert(0, val)
+
+#     def addAtTail(self, val: int) -> None:
+#         self.arr.append(val)
+
+#     def addAtIndex(self, index: int, val: int) -> None:
+#         if index > len(self.arr): return
+#         self.arr.insert(index, val)
+
+#     def deleteAtIndex(self, index: int) -> None:
+#         if index >= len(self.arr): return
+#         self.arr.pop(index)
+
+
 # Your MyLinkedList object will be instantiated and called as such:
 # obj = MyLinkedList()
 # param_1 = obj.get(index)
