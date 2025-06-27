00291503e354a28b3a0788c4ef0ad30f39b5c93d
Mathav Ramalingam <158606381+mathav-ramalingam@users.noreply.github.com>
2025-06-27 08:59:49 +0530

    Time: 3 ms (69.81%), Space: 19.7 MB (8.05%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.py
+++ 0137-single-number-ii/0137-single-number-ii.py
@@ -1,14 +1,26 @@
+from collections import Counter
+
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        val = set()
-        check = []
 
-        for i in nums:
-            if i not in val:
-                val.add(i)
-            else:
-                check.append(i)
+        # # my idealogy
+
+        # val = set()
+        # check = []
+
+        # for i in nums:
+        #     if i not in val:
+        #         val.add(i)
+        #     else:
+        #         check.append(i)
         
-        for v in val:
-            if v not in check:
-                return v
+        # for v in val:
+        #     if v not in check:
+        #         return v
+
+        # using counter class
+        freq = Counter(nums)
+
+        for key , count in freq.items():
+            if count == 1:
+                return key
