07123ec8647f5b096c040b89bc4c1fdff9c78f15
Mathav Ramalingam <158606381+mathav-ramalingam@users.noreply.github.com>
2025-06-28 20:39:31 +0530

    Time: 4 ms (28.23%), Space: 18 MB (68.81%) - LeetHub

---
+++ 2331-intersection-of-multiple-arrays/2331-intersection-of-multiple-arrays.py
@@ -0,0 +1,10 @@
+class Solution:
+    def intersection(self, nums: List[List[int]]) -> List[int]:
+        res = set(nums[0])
+
+        for i in range(1, len(nums)):
+            res = res & set(nums[i])
+        res = list(res)
+        res.sort()
+
+        return res
