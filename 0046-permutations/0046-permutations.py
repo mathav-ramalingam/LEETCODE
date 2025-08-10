dde05640153e515f6924c2d7acad9d726af4a139
Mathav Ramalingam <158606381+mathav-ramalingam@users.noreply.github.com>
2025-08-10 18:52:48 +0530

    Time: 0 ms (100%), Space: 17.9 MB (57.91%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,6 +1,26 @@
-from itertools import permutations
+# using built-in func
+
+# from itertools import permutations
+
+# class Solution:
+#     def permute(self, nums: List[int]) -> List[List[int]]:
+#         perm = [p for p in permutations(nums)]
+#         return perm
+
+
 
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        perm = [p for p in permutations(nums)]
+        perm = []
+        def helper(nums, start, end):
+            if start == end:
+                perm.append(nums[:])
+                return
+            
+            for i in range(start,end+1):
+                nums[start] , nums[i] = nums[i], nums[start]
+                helper(nums,start+1,end)
+                nums[start], nums[i] = nums[i], nums[start]
+
+        helper(nums,0,len(nums)-1)
         return perm
