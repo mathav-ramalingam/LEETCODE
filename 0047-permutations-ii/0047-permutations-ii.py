8b5e78a17c2b0fb1a13cbf83add861f54dab3257
Mathav Ramalingam <158606381+mathav-ramalingam@users.noreply.github.com>
2025-08-12 18:39:31 +0530

    Time: 7 ms (54.87%), Space: 17.9 MB (78.42%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.py
+++ 0047-permutations-ii/0047-permutations-ii.py
@@ -1,17 +1,26 @@
+from itertools import permutations
+
 class Solution:
     def permuteUnique(self, nums: List[int]) -> List[List[int]]:
-        perm = []
 
-        def helper(nums, start, end):
-            if start == end:
-                if nums[:] not in perm:
-                    perm.append(nums[:])
+        # perm = []
+
+        # def helper(nums, start, end):
+        #     if start == end:
+        #         if nums[:] not in perm:
+        #             perm.append(nums[:])
             
-            for i in range(start,end+1):
-                nums[start] , nums[i] = nums[i], nums[start]
-                helper(nums, start+1, end)
-                nums[start], nums[i] = nums[i] , nums[start]
+        #     for i in range(start,end+1):
+        #         nums[start] , nums[i] = nums[i], nums[start]
+        #         helper(nums, start+1, end)
+        #         nums[start], nums[i] = nums[i] , nums[start]
         
 
-        helper(nums,0,len(nums)-1)
-        return perm
+        # helper(nums,0,len(nums)-1)
+        # return perm
+
+        perm = set(permutations(nums))
+
+        return [p for p in perm]
+
+
