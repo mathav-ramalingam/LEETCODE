86350a9ff7a81a2830458ec2e78eb4d1fdf9c33e
Mathav Ramalingam <158606381+mathav-ramalingam@users.noreply.github.com>
2025-09-30 08:43:29 +0530

    Time: 602 ms (31.44%), Space: 0B (100%) - LeetHub

--- 1278-product-price-at-a-given-date/1278-product-price-at-a-given-date.sql
+++ 1278-product-price-at-a-given-date/1278-product-price-at-a-given-date.sql
@@ -8,7 +8,11 @@
 
 
 SELECT p1.product_id,COALESCE(
-    (SELECT p2.new_price FROM Products as p2 WHERE p2.product_id = p1.product_id AND p2.change_date <= "2019-08-16" ORDER BY p2.change_date DESC LIMIT 1),10)as price
+    (SELECT p2.new_price 
+    FROM Products as p2 
+    WHERE p2.product_id = p1.product_id AND p2.change_date <= "2019-08-16" 
+    ORDER BY p2.change_date DESC 
+    LIMIT 1),10)as price
 FROM (SELECT DISTINCT product_id FROM Products) as p1;
 
 
