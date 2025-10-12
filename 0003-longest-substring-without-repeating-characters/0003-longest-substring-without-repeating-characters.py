cfc70ff079be2481804cd2170b4b9e467342cee0
Mathav Ramalingam <158606381+mathav-ramalingam@users.noreply.github.com>
2025-10-12 11:20:10 +0530

    Time: 23 ms (29.39%), Space: 17.8 MB (82.1%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -1,25 +1,41 @@
+# class Solution:
+#     def lengthOfLongestSubstring(self, s: str) -> int:
+#         max_len = 0
+#         i =0 
+
+#         while i < len(s):
+#             j = i
+
+#             while j < len(s):
+#                 sub_str = s[i:j+1]
+
+#                 if len(sub_str) == len(set(sub_str)):
+#                     max_len = len(sub_str) if len(sub_str) > max_len else max_len
+#                 else:
+#                     break
+                
+#                 j += 1
+#             i += 1
+    
+#         return max_len
+
+
+
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
+        sub_str = []
         max_len = 0
-        i =0 
 
-        while i < len(s):
-            j = i
+        for ch in s:
+            if ch in sub_str:
+                idx = sub_str.index(ch)
+                sub_str = sub_str[idx+1:]
+                # print(sub_str)
+            sub_str.append(ch)
+            max_len = max(max_len,len(sub_str))
 
-            while j < len(s):
-                sub_str = s[i:j+1]
-
-                if len(sub_str) == len(set(sub_str)):
-                    max_len = len(sub_str) if len(sub_str) > max_len else max_len
-                else:
-                    break
-                
-                j += 1
-            
-            i += 1
-    
+        
         return max_len
-
 
         
 
