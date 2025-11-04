db6f051287d6cf48386b7c089dcd58291a499fe7
Mathav Ramalingam <158606381+mathav-ramalingam@users.noreply.github.com>
2025-11-04 11:05:56 +0530

    Time: 1186 ms (88.25%), Space: 0B (100%) - LeetHub

--- 0607-sales-person/0607-sales-person.sql
+++ 0607-sales-person/0607-sales-person.sql
@@ -1,11 +1,18 @@
 # Write your MySQL query statement below
 
-SELECT name 
-FROM SalesPerson
-WHERE name NOT IN (SELECT sp.name
-FROM Salesperson as sp 
-JOIN Orders as o
-ON sp.sales_id = o.sales_id 
-JOIN Company as c
-ON o.com_id = c.com_id
-WHERE c.name LIKE "RED")
+-- SELECT name 
+-- FROM SalesPerson
+-- WHERE name NOT IN (SELECT sp.name
+-- FROM Salesperson as sp 
+-- JOIN Orders as o
+-- ON sp.sales_id = o.sales_id 
+-- JOIN Company as c
+-- ON o.com_id = c.com_id
+-- WHERE c.name LIKE "RED")
+
+
+SELECT sp.name
+FROM Orders as o
+JOIN Company as c ON c.com_id = o.com_id AND c.name  = 'RED'
+RIGHT JOIN SalesPerson as sp ON o.sales_id = sp.sales_id
+WHERE o.sales_id is NULL
