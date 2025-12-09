ca9b02ab743ab7553f83745932e96557cfd4c294
Mathav Ramalingam <158606381+mathav-ramalingam@users.noreply.github.com>
2025-12-09 19:07:23 +0530

    Time: 1 ms (88.3%), Space: 18.2 MB (75.95%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.py
+++ 2418-sort-the-people/2418-sort-the-people.py
@@ -1,6 +1,10 @@
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
-        
-        hashset = list(zip(names,heights))
-        sorted_hashset = sorted(hashset, key = lambda x : x[1], reverse = True)
-        return [name for name,_ in sorted_hashset]
+
+        # method 1
+        # hashset = list(zip(names,heights))
+        # sorted_hashset = sorted(hashset, key = lambda x : x[1], reverse = True)
+        # return [name for name,_ in sorted_hashset]
+
+        # method 2:
+        return [name for _,name in sorted(zip(heights,names), reverse = True)]
